91fd189c62e24c91d6311dc1f954f39081c2d818
liuzixi <1227491292@qq.com>
2019-03-25 21:21:34 +0800

    Correct the grammar

--- Container_And_Lift.ino
+++ Container_And_Lift.ino
@@ -1,7 +1,14 @@
 #include <Arduino.h>
 
-int MotorA1 = 3, MotorA2 = 4, MotorB1 = 5, MotorB2 = 6, MotorC1 = 7, MotorC2 = 8;
-int MA_Delaytime = 7200,MB_Delaytime=2800;
+uint8_t MotorA1 = 3;
+uint8_t MotorA2 = 4;
+uint8_t MotorB1 = 5;
+uint8_t MotorB2 = 6;
+uint8_t MotorC1 = 7;
+uint8_t MotorC2 = 8;
+unsigned long MA_Delaytime = 7200;
+unsigned long MB_Delaytime = 2800;
+
 void setup() {
     pinMode(MotorA1, OUTPUT);
     pinMode(MotorA2, OUTPUT);
@@ -14,22 +21,22 @@ void setup() {
 
 void loop() {
     //To control the container push the goods to the car.
-    digitalWrite(MotorA1,HIGH);
-    digitalWrite(MotorA2,LOW);
+    digitalWrite(MotorA1, HIGH);
+    digitalWrite(MotorA2, LOW);
     delay(MA_Delaytime);
-    digitalWrite(MotorA1,LOW);
-    digitalWrite(MotorA2,HIGH);
+    digitalWrite(MotorA1, LOW);
+    digitalWrite(MotorA2, HIGH);
     delay(MA_Delaytime);
-    digitalWrite(MotorA2,LOW);
+    digitalWrite(MotorA2, LOW);
 
     //To control the container push the goods from a car to another container.
-    digitalWrite(MotorB1,HIGH);
-    digitalWrite(MotorB2,LOW);
+    digitalWrite(MotorB1, HIGH);
+    digitalWrite(MotorB2, LOW);
     delay(MB_Delaytime);
-    digitalWrite(MotorB1,LOW);
-    digitalWrite(MotorB2,HIGH);
+    digitalWrite(MotorB1, LOW);
+    digitalWrite(MotorB2, HIGH);
     delay(MB_Delaytime);
-    digitalWrite(MotorB2,LOW);
+    digitalWrite(MotorB2, LOW);
 
     //To control the lift go to the second floor.
     //TODO:Control the lift go to the second floor
